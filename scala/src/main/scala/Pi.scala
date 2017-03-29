08923f23c69c7168bc4970b5c3aeb54d277fc103
Tobin Yehle <tobinyehle@gmail.com>
2017-03-29 15:08:47 -0600

    update the scala solution

--- scala/src/main/scala/Pi.scala
+++ scala/src/main/scala/Pi.scala
@@ -1,12 +1,31 @@
 import scala.util.Random
+import scala.annotation.tailrec
 
 object Pi {
+
+  @tailrec
+  @inline
+  def numOut(r: Random, n: Int, out: Int): Int = {
+    if(n > 0) {
+      val x = r.nextDouble() * 2 - 1
+      val y = r.nextDouble() * 2 - 1
+      if(Math.sqrt(x*x + y*y) > 1) {
+        return numOut(r, n - 1, out + 1)
+      } else {
+        return numOut(r, n - 1, out)
+      }
+    } else {
+      return out
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val n = 10000000
 
-    var out = 0
-
     val r = new Random(42)
+
+    // var out = numOut(r, n, 0)
+    var out = 0
 
     (0 to n) foreach { _ =>
       val y = r.nextDouble() * 2 - 1
