734364069ed13e4fcff480f28379289937778ab7
Tobin Yehle <tobinyehle@gmail.com>
2017-05-16 12:54:21 -0600

    add the python optimizer options

--- time.sh
+++ time.sh
@@ -4,12 +4,13 @@ declare -a programs=("./c/gnu-default" "./c/gnu-1" "./c/gnu-2" "./c/gnu-3" "./c/
                      "./c/clang-default" "./c/clang-1" "./c/clang-2" "./c/clang-3"
                      "java -cp java Pi"
                      "python3 python/pi_1.py" "python3 python/pi_2.py" "python3 python/pi_3.py" "python3 python/pi_4.py"
+                     "python3 -O python/pi_1.py" "python3 -O python/pi_2.py" "python3 -O python/pi_3.py" "python3 -O python/pi_4.py"
                      "pypy3 python/pi_1.py" "pypy3 python/pi_2.py" "pypy3 python/pi_3.py" "pypy3 python/pi_4.py"
                      "java -jar scala/pi-for.jar" "java -jar scala/pi-rec.jar" "java -jar scala/pi-while.jar"
                      "scala -cp scala PiFor" "scala -cp scala PiRec" "scala -cp scala PiWhile")
 
 output=data.tsv
-n=500
+n=2
 
 rm -f $output
 
